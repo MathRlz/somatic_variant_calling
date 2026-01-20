3d3333b99a88605511b51c8c60f0727e91388abb
Krystian Chmielewski <krychatko@gmail.com>
2026-01-20 21:18:56 +0100

    variant calling

--- variant_calling/bqsr.sh
+++ variant_calling/bqsr.sh
@@ -6,6 +6,11 @@ RECAL_TABLES_DIR="recal_tables"
 REFERENCE="reference/GRCh38_reference.fa"
 KNOWN_SITES="reference/All_20180418.vcf.gz"
 
+# Set number of processors (for informational purposes only - BQSR has limited threading benefit)
+if [ -z "$NUM_PROCESSORS" ]; then
+    NUM_PROCESSORS=2
+fi
+
 mkdir -p "$RECAL_DIR" "$RECAL_TABLES_DIR"
 
 for bam in "$RG_DIR"/*_rg.bam; do
@@ -13,14 +18,14 @@ for bam in "$RG_DIR"/*_rg.bam; do
     echo "Processing $sample..."
     
     # Step 1: Build recalibration table
-    gatk BaseRecalibrator \
+    gatk --java-options "-Xmx4g -XX:ParallelGCThreads=2" BaseRecalibrator \
         -I "$bam" \
         -R "$REFERENCE" \
         --known-sites "$KNOWN_SITES" \
         -O "$RECAL_TABLES_DIR/${sample}_recal_data.table"
-    
+
     # Step 2: Apply recalibration
-    gatk ApplyBQSR \
+    gatk --java-options "-Xmx4g -XX:ParallelGCThreads=2" ApplyBQSR \
         -I "$bam" \
         -R "$REFERENCE" \
         --bqsr-recal-file "$RECAL_TABLES_DIR/${sample}_recal_data.table" \
--- variant_calling/call_variants.sh
+++ variant_calling/call_variants.sh
@@ -6,6 +6,11 @@ RECAL_DIR="bam_recalibrated"
 VCF_DIR="vcfs"
 REFERENCE="reference/GRCh38_reference.fa"
 
+# Set number of processors (default to 4 if not set - optimal for Mutect2)
+if [ -z "$NUM_PROCESSORS" ]; then
+    NUM_PROCESSORS=4
+fi
+
 mkdir -p "$VCF_DIR"
 
 # Define tumor-normal pairs
@@ -19,15 +24,16 @@ for tumor in "${!PAIRS[@]}"; do
     normal="${PAIRS[$tumor]}"
     pair_name="${tumor%-T}"
     
-    echo "Calling variants for pair: $pair_name"
-    
-    gatk Mutect2 \
+    echo "Calling variants for pair: $pair_name (using $NUM_PROCESSORS threads)"
+
+    gatk --java-options "-Xmx4g -XX:ParallelGCThreads=2" Mutect2 \
         -R "$REFERENCE" \
         -I "$RECAL_DIR/${tumor}_recalibrated.bam" \
         -I "$RECAL_DIR/${normal}_recalibrated.bam" \
         -normal "${normal}" \
         -O "$VCF_DIR/${pair_name}_raw.vcf.gz" \
-        --f1r2-tar-gz "$VCF_DIR/${pair_name}_f1r2.tar.gz"
+        --f1r2-tar-gz "$VCF_DIR/${pair_name}_f1r2.tar.gz" \
+        --native-pair-hmm-threads "$NUM_PROCESSORS"
     
     echo "Completed $pair_name"
 done
